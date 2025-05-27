2d95077f6b0b987a3ed2fe630e7e09ff49f5b092
kimhyeok_mac <kimmang940715@naver.com>
2025-05-27 16:01:03 +0900

    NullPointerException 추가

--- untitled/src/Hello.java
+++ untitled/src/Hello.java
@@ -1,6 +1,12 @@
 public class Hello {
         public static void main(String[] args) {
-            System.out.println("안녕하세요!");
+            int[] intArray = null;
+            //intArray[0] = 10;
+            // NullPointerException
+
+            String str = null;
+            System.out.println("총 문자 수 : " + str.length());
+            // NullPointerException
         }
     }
 
