7035173f1a6357a5e1b1a11ce4b08fa1b8f7cd5b
DESKTOP-1RVNVF6\lisner <kimmang940715@naver.com>
2025-05-29 10:38:40 +0900

    서버 코드 생성

--- untitled/src/Hello.java
+++ untitled/src/Hello.java
@@ -1,13 +1,68 @@
-public class Hello {
-        public static void main(String[] args) {
-            int[] intArray = null;
-            //intArray[0] = 10;
-            // NullPointerException
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
-            String str = null;
-            System.out.println("총 문자 수 : " + str.length());
-            // NullPointerException
+public class Hello {
+    private static ServerSocket serverSocket = null;
+
+    public static void main(String[] args) {
+        System.out.println("---------------------------");
+        System.out.println("--서버 종료 Q + Enter -------");
+        System.out.println("---------------------------");
+        // TCP 서버 시작
+        startServer();
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String key = scanner.nextLine();
+            if (key.toLowerCase().equals("q")) {
+                break;
+            }
         }
+
+        scanner.close();
+
+        stopServer();
     }
 
+    public static void startServer() {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    // ServerSocket 생성 및 Port 바인딩
+                    serverSocket = new ServerSocket(50001);
+                    System.out.println("[서버] 시작됨");
 
+                    while (true) {
+                        System.out.println("\n[서버] 연결 요청을 기다림\n");
+                        Socket socket = serverSocket.accept();
+
+                        InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+                        System.out.println("[서버] " + isa.getHostName() + "의 연결 요청을 수락함");
+
+                        // 연결 끊기
+                        socket.close();
+                        System.out.println("[서버] " + isa.getHostName() + "의 연결을 끊음");
+                    }
+                } catch (IOException e) {
+                    System.out.println("[서버] " + e.getMessage());
+                }
+            }
+        };
+        thread.start();
+    }
+
+    public static void stopServer() {
+        if (serverSocket != null && !serverSocket.isClosed()) {
+            try {
+                serverSocket.close();
+                System.out.println("[서버] 종료됨");
+            } catch (IOException e) {
+                System.out.println("[서버] 종료 중 오류: " + e.getMessage());
+            }
+        }
+    }
+}
