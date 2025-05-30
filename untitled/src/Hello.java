9fd93425934f5535e208548f0ebcb5a9025368d1
kimhyeok_mac <kimmang940715@naver.com>
2025-05-31 03:18:05 +0900

    자바연습

--- untitled/src/Hello.java
+++ untitled/src/Hello.java
@@ -1,12 +1,30 @@
 public class Hello {
         public static void main(String[] args) {
-            int[] intArray = null;
-            //intArray[0] = 10;
-            // NullPointerException
+            String strVar1 = "홍길동";
+            String strVar2 = "홍길동";
 
-            String str = null;
-            System.out.println("총 문자 수 : " + str.length());
-            // NullPointerException
+            if(strVar1 == strVar2) {
+                System.out.println("strVar1과 strVar2는 참조가 같음");
+            } else {
+                System.out.println("strVar1과 strVar2는 참조가 다름");
+            }
+
+            if (strVar1.equals(strVar2)) {
+                System.out.println("strVar1과 strVar2는 문자열이 같음");
+            }
+
+            String strVar3 = new String("홍길동");
+            String strVar4 = new String("홍길동");
+
+            if(strVar3 == strVar4) {
+                System.out.println("strVar3과 strVar4는 참조가 같음");
+            } else {
+                System.out.println("strVar3과 strVar4는 참조가 다름");
+            }
+
+            if (strVar3.equals(strVar4)) {
+                System.out.println("strVar3과 strVar4는 문자열이 같음");
+            }
         }
     }
 
