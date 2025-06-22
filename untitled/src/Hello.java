e518a789291ee0c1b4a584e4ce0884ce2601270d
kimmang94 <kimmang940715@naver.com>
2025-06-23 01:33:41 +0900

    객체지향 시작

--- untitled/src/Hello.java
+++ untitled/src/Hello.java
@@ -1056,7 +1056,26 @@ public class Hello {
         }
 
     }
-    
+
+    public class SportCar {
+
+    }
+
+    class Tire {
+
+    }
+
+    public static class Student {
+
+    }
+
+    public static void funcTest54() {
+        Student s1 = new Student();
+        System.out.println("s1 변수가 Student 객체를 참조합니다");
+
+        Student s2 = new Student();
+        System.out.println("s2 변수가 또 다른 Student 객체를 잠조합니다");
+    }
 }
 
 
